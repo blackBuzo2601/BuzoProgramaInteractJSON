2fdd38d6febf6a20235024ce4ab9bd180ca60e62
blackBuzo2601 <elianpro58@gmail.com>
2024-02-15 16:08:19 -0800

    Cascaron para algoritmo

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ Elaborado por Buzo Zamora Elian
 14 de Febrero de 2024, Ensenada B.C
 */ 
 
+/* //Sintaxis de objeto en JS
+var objeto = {      
+    1: "uno",
+    2: "dos",
+    3: "tres",
+    4: "cuatro"
+}
+*/
+
 /*  Usamos el modulo fs de Node.js
     con require ('fs) se esta importando el modulo 
     para acceder a todas las funciones y metodos de fs. */
-
 const fs = require ('fs'); 
     /*readFile es un método de fs.
     UTF-8 el parámetro que indica que se desea leer el archivo en formato UTF-8. 
@@ -17,11 +25,14 @@ const fs = require ('fs');
     data: se utiliza para almacenar los datos o el resultado de la operacion
     */ 
 
-    fs.readFile('colores1.json','utf8', (err, data) => {
-        var informacion= data;                    
-        console.log("IMPRESIÓN DE LA INFORMACIÓN\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n");
-        console.log(informacion);
+fs.readFile('colores1.json','utf8', (err, data) => { //todo el código aqui.
+        console.log("\n\n\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠");
+        console.log("Bienvenido usuario: Este es un algoritmo para leer JSON.");
         
-        var largoInformacion=data.length;
-        console.log("El largo de la informacion es: "+largoInformacion);
-    });
+
+
+
+
+});  //fin del método readFILE
+
+
