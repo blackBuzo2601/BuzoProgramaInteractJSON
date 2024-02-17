8ccc308d30897482875598b0025067f794e5af3c
blackBuzo2601 <elianpro58@gmail.com>
2024-02-16 17:44:46 -0800

    Codigo mas estable hasta ahora

--- app.js
+++ app.js
@@ -17,113 +17,114 @@ var objeto = {
 /*  Usamos el modulo fs de Node.js
     con require ('fs) se esta importando el modulo 
     para acceder a todas las funciones y metodos de fs. */
-const { info } = require('console');
-const fs = require ('fs'); 
-    /*readFile es un método de fs.
-    UTF-8 el parámetro que indica que se desea leer el archivo en formato UTF-8. 
-    (err, data) son las variables que usamos como parámetro de nuestro callBack.
-    err: se utiliza para almacenar cualquier error que ocurra durante la operacion.
-    data: se utiliza para almacenar los datos o el resultado de la operacion
-    */ 
-
-//Declaración de variables
-const corcheteApertura="{";
-const corcheteCierre="}";
-const comillasDobles='"';
-var posicionComillasDobles1=0;
-const caracterVacio=" ";
-var primeraLetra="";
-  var banderaCorcheteAp=false;
-  var banderaCorcheteCie=false;
-    var banderaClaveCorrecta=false;
-    var banderaLetrasIncluidas=false;
-    var banderaComillasDobles=false;
-
-  var espaciosTotales=0;
-    var posicionCorcheteAp=5; //inicialización de variable (ignorar que es un 5)
-    var formarClave="";
-    const ABCDARIO="abcdefghijklmnopqrstuvwxyzABCDEFGHIZJKLMNOPQRSTUVWXYZ";
-
-fs.readFile('colores1.json','utf8', (err, data) => { //todo el código aqui.
-    console.log("\n\n\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠");
-    console.log("Bienvenido usuario: Este es un algoritmo para leer JSON.");
-    var infoJSON = data; //variable que almacena toda la información del archivo .JSON
-
-    for(let i=0; i<infoJSON.length;i++){ //bucle for (A) que busca el primer caracter en la DATA.
-        if(infoJSON[i]!=caracterVacio){
-            console.log("Caracter encontrado en la posicion: "+i+". Es el caracter: "+infoJSON[i]);
-            primeraLetra=infoJSON[i];
-            posicionPrimerCaracter=i;
-            break; //detener el bucle for en la posición donde se cumplió la condición
-        }
-    }//fin bucle for (A)
-
-
-    //BLOQUE PARA ACTIVAR BANDERA DE CORCHETE APERTURA
-    if(primeraLetra==corcheteApertura){
-        banderaCorcheteAp=true;
-        posicionCorcheteAp=posicionPrimerCaracter;
-        console.log("La posicion del corchete de apertura es: "+posicionCorcheteAp);
-    }else{
-        console.log("Error de sintaxis. No se encontró '{' al inicio del .JSON");
-    }  //fin de condicion encontrar "{""
-   
-//------------BUSCAR QUE DESPUES DEL "{" exista un (")-----------------------------------------------------------
-//♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠CONDICIONAL DEL CORCHETE♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠
-    if(banderaCorcheteAp==true){//lo siguiente del corchete de apertura es buscar (")
-        for(let i=posicionCorcheteAp; i<infoJSON.length;i++){ //bucle for (B) que busca (") despues del {
+    const { info } = require('console');
+    const fs = require ('fs'); 
+        /*readFile es un método de fs.
+        UTF-8 el parámetro que indica que se desea leer el archivo en formato UTF-8. 
+        (err, data) son las variables que usamos como parámetro de nuestro callBack.
+        err: se utiliza para almacenar cualquier error que ocurra durante la operacion.
+        data: se utiliza para almacenar los datos o el resultado de la operacion
+        */ 
+    
+    //Declaración de variables
+    const corcheteApertura="{";
+    const corcheteCierre="}";
+    const comillasDobles='"';
+    var posicionComillasDobles1=0;
+    const caracterVacio=" ";
+    var primeraLetra="";
+      var banderaCorcheteAp=false;
+      var banderaCorcheteCie=false;
+        var banderaClaveCorrecta=false;
+        var banderaLetrasIncluidas=false;
+        var banderaComillasDobles=false;
+    
+      var espaciosTotales=0;
+        var posicionCorcheteAp=5; //inicialización de variable (ignorar que es un 5)
+        var formarClave="";
+        const ABCDARIO="abcdefghijklmnopqrstuvwxyzABCDEFGHIZJKLMNOPQRSTUVWXYZ";
+    
+    fs.readFile('colores1.json','utf8', (err, data) => { //todo el código aqui.
+        console.log("\n\n\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠\n♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠");
+        console.log("Bienvenido usuario: Este es un algoritmo para leer JSON.");
+        var infoJSON = data; //variable que almacena toda la información del archivo .JSON
+    
+        for(let i=0; i<infoJSON.length;i++){ //bucle for (A) que busca el primer caracter en la DATA.
             if(infoJSON[i]!=caracterVacio){
-                if(infoJSON[i]==comillasDobles){ //verificar que el siguiente caracter estrictamente sean comillas dobles
-                    console.log("Comillas dobles encontradas en la posición: "+i);
-                    posicionComillasDobles1=i;
-                    formarClave=formarClave+infoJSON[i]; //almacenar las comillas dobles para la CLAVE
-                    break; //Detener bucle for por completod
-                }
-                else if(ABCDARIO.includes(infoJSON[i])){
-                    console.log("ERROR DE SINTAXIS. No se encontró comillas dobles después de ({). ");
-                    break;
-                }
-            } 
-        }//fin bucle for (B) 
+                console.log("Caracter encontrado en la posicion: "+i+". Es el caracter: "+infoJSON[i]);
+                primeraLetra=infoJSON[i];
+                posicionPrimerCaracter=i;
+                break; //detener el bucle for en la posición donde se cumplió la condición
+            }
+        }//fin bucle for (A)
+    
+    
+        //BLOQUE PARA ACTIVAR BANDERA DE CORCHETE APERTURA
+        if(primeraLetra==corcheteApertura){
+            banderaCorcheteAp=true;
+            posicionCorcheteAp=posicionPrimerCaracter;
+            console.log("La posicion del corchete de apertura es: "+posicionCorcheteAp);
+        }else{
+            console.log("Error de sintaxis. No se encontró '{' al inicio del .JSON");
+        }  //fin de condicion encontrar "{""
+       
+    //------------BUSCAR QUE DESPUES DEL "{" exista un (")-----------------------------------------------------------
+    //♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠CONDICIONAL DEL CORCHETE♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠
+        if(banderaCorcheteAp==true){//lo siguiente del corchete de apertura es buscar (")
+            for(let i=posicionCorcheteAp; i<infoJSON.length;i++){ //bucle for (B) que busca (") despues del {
+                if(infoJSON[i]!=caracterVacio){
 
-
-    for(let i=posicionComillasDobles1+1;i<infoJSON.length;i++){ //bucle for (C) que busca letras del abecedario, +1 para buscar despues del "
-        if(ABCDARIO.includes(infoJSON[i])){ //verificar que sean letras para construir la clave
-            formarClave=formarClave+infoJSON[i];
-        }
-        else if(infoJSON[i]==comillasDobles){ //si no encuentra letras, debe encontrar las comillas dobles.
-            formarClave=formarClave+infoJSON[i];
-            //aqui un condicional que valide que todo esta correcto.
-            banderaClaveCorrecta=true;
-            break;
-        }
-        else if(infoJSON[i]==" "){ //si no encontró las comillas, puede haber un espacio todavía
-            formarClave=formarClave+infoJSON[i];
-        }
-        else{ //si no encontró las comillas dobles, es error de sintaxis porque las comillas son parte de la clave.
-            console.log("Error de sintaxis. No se construyó correctamente la clave: "+formarClave);
-            break;
-        }
-        
-}//fin bucle for (C) 
-  
-
-
-    }//♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠FIN DEL CONDICIONAL DEL CORCHETE♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠
-
-    //Entra en este condicional si la clave fue escrita con la sintaxis correcta.
-    if(banderaClaveCorrecta==true){ //Para que esté en true, tiene que haber entrado en los for anteriores
-        console.log("CLAVE: "+formarClave);
-
-        //LO SIGUIENE, ES ENCONTRAR UN ":" que es el que indica que se asigna un VALOR a la CLAVE.
-
-        
-
-
-    }//fin del condicional si la clave fue escrita con la sintaxis correcta.
-
-
-
-});//////////////////////////////////////fin del método readFILE///////////////////////////////////////////////////
-
-
+                    if(infoJSON[i]==comillasDobles){ //verificar que el siguiente caracter estrictamente sean comillas dobles
+                        console.log("Comillas dobles encontradas en la posición: "+i);
+                        posicionComillasDobles1=i;
+                        formarClave=formarClave+infoJSON[i]; //almacenar las comillas dobles para la CLAVE
+                        break; //Detener bucle for por completod
+                    }else{
+                        console.log("ERROR DE SINTAXIS. No se encontró comillas dobles después de ({). ");
+                        break;
+                    }
+                  
+                } 
+            }//fin bucle for (B) 
+    
+    
+        for(let i=posicionComillasDobles1+1;i<infoJSON.length;i++){ //bucle for (C) que busca letras del abecedario, +1 para buscar despues del "
+            if(ABCDARIO.includes(infoJSON[i])){ //verificar que sean letras para construir la clave
+                formarClave=formarClave+infoJSON[i];
+            }
+            else if(infoJSON[i]==comillasDobles){ //si no encuentra letras, debe encontrar las comillas dobles.
+                formarClave=formarClave+infoJSON[i];
+                //aqui un condicional que valide que todo esta correcto.
+                banderaClaveCorrecta=true;
+                break;
+            }
+            else if(infoJSON[i]==" "){ //si no encontró las comillas, puede haber un espacio todavía
+                formarClave=formarClave+infoJSON[i];
+            }
+            else{ //si no encontró las comillas dobles, es error de sintaxis porque las comillas son parte de la clave.
+                console.log("Error de sintaxis. No se construyó correctamente la clave: "+formarClave);
+                break;
+            }
+            
+    }//fin bucle for (C) 
+      
+    
+    
+        }//♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠FIN DEL CONDICIONAL DEL CORCHETE♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠
+    
+        //Entra en este condicional si la clave fue escrita con la sintaxis correcta.
+        if(banderaClaveCorrecta==true){ //Para que esté en true, tiene que haber entrado en los for anteriores
+            console.log("CLAVE: "+formarClave);
+    
+            //LO SIGUIENE, ES ENCONTRAR UN ":" que es el que indica que se asigna un VALOR a la CLAVE.
+    
+            
+    
+    
+        }//fin del condicional si la clave fue escrita con la sintaxis correcta.
+    
+    
+    
+    });//////////////////////////////////////fin del método readFILE///////////////////////////////////////////////////
+    
+    
